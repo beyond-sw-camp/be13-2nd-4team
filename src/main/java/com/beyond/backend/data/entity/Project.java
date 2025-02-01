1ccf43af8cfcf3ecc2d23253764b3634a01b8dc5
blooper20 <yjw127588@gmail.com>
2025-02-01 22:02:38 +0900

    MOYO-30 [유재우] 프로젝트 Entity 생성

---
+++ src/main/java/com/beyond/backend/data/entity/Project.java
@@ -0,0 +1,39 @@
+package com.beyond.backend.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * <p>
+ *
+ * <p>packageName    : com.beyond.backend.data.entity
+ * <p>fileName       : Project
+ * <p>author         : jaewoo
+ * <p>date           : 2025. 2. 1.
+ * <p>description    : 프로젝트 Entity
+ */
+/*
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 2. 1.        jaewoo             최초 생성
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "project")
+public class Project extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String content;
+}
