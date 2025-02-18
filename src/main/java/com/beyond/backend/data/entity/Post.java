72f6a258b553ec8aafcb4456b05d3825745e5115
hjsong <dev_sklg0602@naver.com>
2025-02-18 17:00:45 +0900

    [송현준] 충돌 해결(테스트 완료)

--- src/main/java/com/beyond/backend/data/entity/Post.java
+++ src/main/java/com/beyond/backend/data/entity/Post.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,7 +18,8 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 당장은 필요없지만 확장성 위해
-    private Long id;
+    @Column(name = "post_no")
+    private Long no;
 
     @Column(nullable = false)
     private String postTitle;
@@ -26,17 +27,37 @@ public class Post {
     @Column(nullable = false)
     private String postContent;
 
-    @Embedded
-    private TimePeriod timePeriod;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BoardType boardType;
+
+    @Embedded
+    private TimePeriod timePeriod;
+
+    // FK
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_no", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Comment> comments = new HashSet<>();
+
+    //============================
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<>();
+
+    //좋아요 연관관계 로직
+    public int getLikeCount() {
+        return likes.size();
+    }
+
+    //    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    //    private List<BookMark> bookmarks;
 }
